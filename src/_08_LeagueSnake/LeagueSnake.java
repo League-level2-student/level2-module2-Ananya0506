66fb885f75034390b55baccbd11f8abc66b9208b
league <league@imac-22.attlocal.net>
2024-10-02 16:31:16 -0700

    done!

--- src/_08_LeagueSnake/LeagueSnake.java
+++ src/_08_LeagueSnake/LeagueSnake.java
@@ -1,5 +1,6 @@
 package _08_LeagueSnake;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
@@ -23,7 +24,7 @@ int FoodX;
 int FoodY;
 int direction = UP;
 int FoodEaten = 0;    
-ArrayList<Segment>[]tail = new ArrayList[FoodEaten];
+ArrayList<Segment>tail = new ArrayList<>();
 
 /*
      * Setup methods
@@ -38,14 +39,14 @@ ArrayList<Segment>[]tail = new ArrayList[FoodEaten];
     @Override
     public void setup() {
         head = new Segment(x, y);
-        frameRate(20);
+        frameRate(15);
         dropFood();
     }
 
     void dropFood() {
         // Set the food in a new random location
-        FoodX = ((int)random(50)*10);
-        FoodY = ((int)random(50)*10);
+        FoodX = ((int)random(25)*20);
+        FoodY = ((int)random(25)*20);
         
     }
 
@@ -58,6 +59,7 @@ ArrayList<Segment>[]tail = new ArrayList[FoodEaten];
     @Override
     public void draw() {
         background(0,0,0);
+      text("Food Eaten: " + FoodEaten, 10,10);
         drawFood();
         move();
         drawSnake();
@@ -67,7 +69,7 @@ ArrayList<Segment>[]tail = new ArrayList[FoodEaten];
     void drawFood() {
         // Draw the food
 fill(0,238,255);
-rect(FoodX, FoodY, 10,10);
+rect(FoodX, FoodY, 20,20);
         		
     }
 
@@ -75,12 +77,17 @@ rect(FoodX, FoodY, 10,10);
         // Draw the head of the snake followed by its tail
     fill(255,200,0);
     rect(x,y,20,20);
-    
+    manageTail();
     }
 
     void drawTail() {
         // Draw each segment of the tail
-    	tail[FoodEaten] = new Segment(10,10);
+    	
+    	for(int i = 0; i<tail.size(); i++) {
+    		rect(tail.get(i).x,tail.get(i).y,20,20); 
+    	}
+ 
+
     	
     }
 
@@ -96,13 +103,22 @@ rect(FoodX, FoodY, 10,10);
         // This produces the illusion of the snake tail moving.
 checkTailCollision();
 drawTail();
+Segment seg  = new Segment(x,y);
+tail.add(seg);
+tail.remove(0);
     }
 
     void checkTailCollision() {
         // If the snake crosses its own tail, shrink the tail back to one segment
-        
+        for(int a = 0; a<tail.size(); a++) {
+        	if(tail.get(a).x == x  && tail.get(a).y == y) {
+        	tail = new ArrayList<>();
+        	FoodEaten = 1;
+        	break;
+        	}
+    
+        }
     }
-
     /*
      * Control methods
      * 
@@ -115,13 +131,13 @@ drawTail();
         // Set the direction of the snake according to the arrow keys pressed
     	int keyCode = e.getKeyCode();
     
-    if (keyCode == 37) {
+    if (keyCode == 37 && direction!= RIGHT) {
     	direction = LEFT;
-    } else if (keyCode == 38) {
+    } else if (keyCode == 38 && direction!=DOWN) {
     	direction = UP;
-    } else if (keyCode == 39) {
+    } else if (keyCode == 39 && direction!= LEFT) {
     	direction = RIGHT;
-    } else if (keyCode == 40) {
+    } else if (keyCode == 40 && direction!= UP) {
     	direction = DOWN;
     }
     
@@ -135,14 +151,14 @@ drawTail();
     
         if (direction == UP) {
             // Move head up
-            y-=5;
+            y-=20;
         } else if (direction == DOWN) {
             // Move head down
-                y+=5;
+                y+=20;
         } else if (direction == LEFT) {
-            x-=5;
+            x-=20;
         } else if (direction == RIGHT) {
-            x+=5;
+            x+=20;
         }
      checkBoundaries();
      
@@ -153,11 +169,11 @@ drawTail();
         if(x>500) {
         	x=0;
         } else if (x<0) {
-        	x=500;
+        	x=480;
         } else if (y>500) {
         	y=0;
         } else if (y<0) {
-        	y=500;
+        	y=480;
         }
     }
 
@@ -167,9 +183,11 @@ drawTail();
         if (x==FoodX && y==FoodY) {
         	FoodEaten++;
         	dropFood();
-        	drawFood();
+        Segment seg2 = new Segment(x,y);
+        	tail.add(seg2);
         }
     }
+    
 
     static public void main(String[] passedArgs) {
         PApplet.main(LeagueSnake.class.getName());
